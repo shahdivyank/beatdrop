a9861afd839336a2c198ed93b1a9658a600755f9
Zorten <zergionaruto@hotmail.com>
2023-01-27 15:50:45 -0800

    Started Welcome component. Still need to resize and relocate images and text. Changed website icon as well

--- components/Welcome.jsx
+++ components/Welcome.jsx
@@ -1,17 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const Welcome = () => {
   return (
-    
-    <div class= "h-screen bg-indigo-500">
-        <img class="w-16" src="beatdrop-logo-white-with.png"/>
-        <img class="w-96" src="beatdrop-white.png"/>
-        <h3 class="text-2xl mb-8 text-gray-200">
-          see the world from another perspective
-        </h3>
+    <div class="h-screen bg-indigo-500">
+      <img class="w-16" src="beatdrop-logo-white-with.png" />
+      <img class="w-96" src="beatdrop-white.png" />
+      <h3 class="text-2xl mb-8 text-gray-200">
+        see the world from another perspective
+      </h3>
     </div>
+  );
+};
 
-  )
-}
-
-export default Welcome
+export default Welcome;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
+import Welcome from "@/components/Welcome";
 import React from "react";
 
 const index = () => {
-  return <div className="text-red-500">index page</div>;
+  return (
+    <div className="text-red-500">
+      <Welcome />
+    </div>
+  );
 };
 
 export default index;
