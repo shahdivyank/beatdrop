9a4198ba99dc6cd7bb553816df8980d0b55380eb
Zorten <zergionaruto@hotmail.com>
2023-01-27 00:24:20 -0800

    Began welcome component. Inserted logo and name pictures, need to rescale and reposition. Also changed the website icon.

---
+++ components/Welcome.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Welcome = () => {
+  return (
+    
+    <div class= "h-screen bg-indigo-500">
+        <img class="w-16" src="beatdrop-logo-white-with.png"/>
+        <img class="w-96" src="beatdrop-white.png"/>
+        <h3 class="text-2xl mb-8 text-gray-200">
+          see the world from another perspective
+        </h3>
+    </div>
+
+  )
+}
+
+export default Welcome
