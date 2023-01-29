0e7055d4266dd99197967f53b22c6bccc2ac71c5
Mariam Golwalla <mgolw001@ucr.edu>
2023-01-28 16:44:00 -0800

    adequate navbar

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
 import React from "react";
+import Navigation from "@/components/Navigation";
 
 const index = () => {
-  return <div className="text-beatdrop-teal">index page</div>;
+  return (
+    <div className="text-beatdrop-teal">
+      index page
+      <Navigation />
+    </div>
+  );
 };
 
 export default index;
