0d97ad164ddb06366371fcc6566a576cf9d219fc
Zorten <zergionaruto@hotmail.com>
2023-01-29 20:38:31 -0800

    Finished aligning images and text. Component is finished.

--- components/Welcome.jsx
+++ components/Welcome.jsx
@@ -3,11 +3,21 @@ import React from "react";
 const Welcome = () => {
   return (
     <div class="h-screen bg-indigo-500">
-      <img class="w-16" src="beatdrop-logo-white-with.png" />
-      <img class="w-96" src="beatdrop-white.png" />
-      <h3 class="text-2xl mb-8 text-gray-200">
-        see the world from another perspective
-      </h3>
+
+      <div class="flex justify-center items-center pt-24">  
+        <img class="w-20" src="beatdrop-logo-white-with.png" />
+      </div>
+
+      <div class="flex justify-center items-center pt-4">
+      <img class="w-1/2" src="beatdrop-white.png" />
+      </div>
+
+
+      <div class="flex justify-center items-center">
+        <h3 class="text-2xl mb-8 text-gray-200">see the world from another perspective</h3>
+      </div>
+
+
     </div>
   );
 };
