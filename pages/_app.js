5821203c19fba59ff2d467aa68864f4c6f894254
Robert Lerias <72523111+bobbyyy57@users.noreply.github.com>
2023-01-30 23:49:04 -0800

    added eslint-disable new-cap

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
 import "@/styles/globals.css";
+
+/* eslint-disable new-cap */
 import { Outfit } from "@next/font/google";
 
 const outfit = Outfit({ subsets: ["latin"], variable: "--font-outfit" });
