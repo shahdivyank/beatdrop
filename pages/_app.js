1938277c1567bc077a7f62b71ebc3691185d0822
Robert Lerias <72523111+bobbyyy57@users.noreply.github.com>
2023-01-30 23:43:43 -0800

    Added Outfit Font

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
 import "@/styles/globals.css";
+import { Outfit } from "@next/font/google";
+
+const outfit = Outfit({ subsets: ["latin"], variable: "--font-outfit" });
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={outfit.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
