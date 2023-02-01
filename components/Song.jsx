a9ec277ff0bfe5022e76e4ef599f255e4f2238f3
Rodrigo <rddlamas@gmail.com>
2023-02-01 01:16:18 -0800

    Song component finished

--- components/Song.jsx
+++ components/Song.jsx
@@ -3,13 +3,11 @@ import img from "../public/Ditto.jpg";
 import Image from "next/image";
 import { HiArrowNarrowRight } from "react-icons/hi";
 
-
-const fontStyles = {color: '#cbd5e1', fontSize: '70px'};
+const fontStyles = { color: "#cbd5e1", fontSize: "70px" };
 
 const Song = ({ band, song, username, time }) => {
   return (
-   
-   <div className = "flex items-center gap-4">
+    <div className="flex items-center gap-4">
       <Image
         src={img}
         placeholder="blur"
@@ -18,20 +16,19 @@ const Song = ({ band, song, username, time }) => {
         width="150"
         height="280"
         className="rounded-full"
-        
       />
       <div>
-
-        <p className="text-2xl font-bold  m-0">{" "} {band} - {song}{" "} </p>
+        <p className="text-2xl font-bold  m-0">
+          {" "}
+          {band} - {song}{" "}
+        </p>
         <p className="text-lg text-bold m-0 "> Posted by {username}</p>
         <p className="text-sm text-bold m-0">{time} AGO</p>
-
-      </div>
-      
-      <div >
-      < HiArrowNarrowRight style={fontStyles}/>
       </div>
 
+      <div>
+        <HiArrowNarrowRight style={fontStyles} />
+      </div>
     </div>
   );
 };
