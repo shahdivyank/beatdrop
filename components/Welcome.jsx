df2446703d5a6ec678509931e02d5beb4a9a250d
Zorten <zergionaruto@hotmail.com>
2023-02-01 17:25:00 -0800

    Changed class to className to fix small warning

--- components/Welcome.jsx
+++ components/Welcome.jsx
@@ -4,17 +4,17 @@ import logoPic from "../public/beatdrop-logo-white-text.png";
 import headphonePic from "../public/beatdrop-logo-white-headphones.png";
 const Welcome = () => {
   return (
-    <div class="h-screen bg-indigo-500">
-      <div class="flex justify-center items-center pt-24">
+    <div className="h-screen bg-indigo-500">
+      <div className="flex justify-center items-center pt-24">
         <Image src={headphonePic} alt="headphone logo" width={56} height={56} />
       </div>
 
-      <div class="flex justify-center items-center pt-4">
+      <div className="flex justify-center items-center pt-4">
         <Image src={logoPic} alt="logo pic" width={512} height={512} />
       </div>
 
-      <div class="flex justify-center items-center">
-        <h3 class="text-2xl mb-8 text-gray-200">
+      <div className="flex justify-center items-center">
+        <h3 className="text-2xl mb-8 text-gray-200">
           hear the world from another perspective
         </h3>
       </div>
