52158b59e323c02d1a329f05630a3fd660d3a11f
Rodrigo <rddlamas@gmail.com>
2023-02-01 17:51:46 -0800

    commit for divyank:)

---
+++ pages/404.js
@@ -0,0 +1,9 @@
+// pages/404.js
+export default function Custom404() {
+  return (
+    <div className="flex bg-red-500 items-center justify-center">
+      <p className="text-8xl font-bold  m-0">404</p>
+      <p className="text-lg font-bold  m-0">oops...page not found!</p>
+    </div>
+  );
+}
