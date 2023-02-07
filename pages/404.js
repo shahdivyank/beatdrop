ff24b5989d0b817c860732e65c4af7b222731a49
Rodrigo <rddlamas@gmail.com>
2023-02-06 22:23:52 -0800

    Finished 404 page implementation

--- pages/404.js
+++ pages/404.js
@@ -3,8 +3,8 @@ export default function Custom404() {
   return (
     <div className="h-screen flex bg-red-500 items-center justify-center">
       <div className="flex flex-col items-center justify-center w-[480px] h-[480px] rounded-full bg-gray-300">
-        <p className=" text-8xl font-bold m-0">404</p>
-        <p className=" text-lg font-bold  m-0">oops...page not found!</p>
+        <p className=" text-9xl font-bold m-0">404</p>
+        <p className=" text-mg font-bold  m-0">oops...page not found!</p>
       </div>
     </div>
   );
