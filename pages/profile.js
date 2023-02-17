e0ca2497556a2c896150537ecfe04f2db813f99a
Mariam Golwalla <mgolw001@ucr.edu>
2023-02-17 01:55:01 -0800

    uppercase titles

--- pages/profile.js
+++ pages/profile.js
@@ -4,7 +4,7 @@ import React from "react";
 const profile = () => {
   return (
     <div className="bg-beatdrop-lightgrey">
-      <title> profile </title>
+      <title>Profile </title>
       profile
       <Profiledrops />
     </div>
