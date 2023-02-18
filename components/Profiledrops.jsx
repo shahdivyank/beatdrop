4fa6e9212e71edebff3a924ded11faac4fea5773
Rodrigo <rddlamas@gmail.com>
2023-02-17 20:54:34 -0800

    something

--- components/Profiledrops.jsx
+++ components/Profiledrops.jsx
@@ -1,21 +1,13 @@
 import React from "react";
-import Profiledrop from "./Profiledrop";
-import PinkFriday from "../public/Pink_Friday_album_cover.jpg";
 
-const Profiledrops = () => {
+const ProfileDrops = () => {
+  // const [toggle, setToggle] = useState(0);
+
   return (
-    <div>
-      <Profiledrop
-        number={1}
-        pic={PinkFriday}
-        song="Super Bass - Nicki Minaj"
-        hours="24"
-        comments={7}
-        likes={23}
-        location="Riverside, California"
-      />
+    <div className="p-4">
+      <p>Test test test</p>
     </div>
   );
 };
 
-export default Profiledrops;
+export default ProfileDrops;
