6efb1496a5401ec6cd0a08f74706f5595822acaa
Divyank Shah <divyank.shah.2016@gmail.com>
2023-02-20 00:45:21 -0800

    connect view and listing

--- components/Listing.jsx
+++ components/Listing.jsx
@@ -1,12 +1,79 @@
 import React, { useState } from "react";
 import Song from "@/components/Song.jsx";
+import View from "./View";
+
+const songs = [
+  {
+    song: "Bob B",
+    band: "Rodrigo",
+    time: "1 HOUR",
+    username: "Bobby",
+    location: "Riverside, CA",
+    description: "POGGERS DESCRIPTION",
+  },
+  {
+    song: "Bob B",
+    band: "Rodrigo",
+    time: "1 HOUR",
+    username: "Bobby",
+    location: "Riverside, CA",
+    description: "POGGERS DESCRIPTION",
+  },
+  {
+    song: "Bob B",
+    band: "Rodrigo",
+    time: "1 HOUR",
+    username: "Bobby",
+    location: "Riverside, CA",
+    description: "POGGERS DESCRIPTION",
+  },
+  {
+    song: "Bob B",
+    band: "Rodrigo",
+    time: "1 HOUR",
+    username: "Bobby",
+    location: "Riverside, CA",
+    description: "POGGERS DESCRIPTION",
+  },
+  {
+    song: "Bob B",
+    band: "Rodrigo",
+    time: "1 HOUR",
+    username: "Bobby",
+    location: "Riverside, CA",
+    description: "POGGERS DESCRIPTION",
+  },
+];
 
 const Listing = () => {
   const [toggle, setToggle] = useState(0);
+  const [toggleView, setToggleView] = useState(false);
+  const [song, setSong] = useState({
+    song: "",
+    band: "",
+    time: "",
+    username: "",
+    location: "",
+  });
+
+  const toggleViewHandler = (song) => {
+    setSong(song);
+    setToggleView(true);
+  };
 
   return (
     <>
       <div className="flex justify-end">
+        {toggleView && (
+          <View
+            song={song.name}
+            description={song.description}
+            location={song.location}
+            posted={song.posted}
+            album="https://upload.wikimedia.org/wikipedia/en/7/7b/Chungha_-_Querencia.jpg"
+            setToggleView={setToggleView}
+          />
+        )}
         <div className="bg-beatdrop-lightgrey w-3.5/12 h-1/2 rounded-4xl py-4 px-3 my-10 mr-10 flex flex-col items-center">
           <div className=" text-white text-sm rounded-full w-11/12">
             <div className="flex justify-evenly">
@@ -38,38 +105,20 @@ const Listing = () => {
           </div>
 
           <div className="my-4 px-2">
-            <div className="border-b-2 border-[#E3E3E3]">
-              <Song
-                song="Bob B"
-                band="Rodrigo"
-                time="1 HOUR"
-                username="Bobby"
-              />
-            </div>
-            <div className="border-b-2 border-[#E3E3E3]">
-              <Song
-                song="Bob B"
-                band="Rodrigo"
-                time="1 HOUR"
-                username="Bobby"
-              />
-            </div>
-            <div className="border-b-2 border-[#E3E3E3]">
-              <Song
-                song="Bob B"
-                band="Rodrigo"
-                time="1 HOUR"
-                username="Bobby"
-              />
-            </div>
-            <div className="border-b-2 border-[#E3E3E3]">
-              <Song
-                song="Bob B"
-                band="Rodrigo"
-                time="1 HOUR"
-                username="Bobby"
-              />
-            </div>
+            {songs.map((song, index) => (
+              <div
+                className="border-b-2 border-[#E3E3E3]"
+                key={index}
+                onClick={() => toggleViewHandler(song)}
+              >
+                <Song
+                  song={song.song}
+                  band={song.band}
+                  time={song.time}
+                  username={song.username}
+                />
+              </div>
+            ))}
           </div>
 
           <button className="hover:bg-beatdrop-yellow hover:text- bg-beatdrop-pink text-white text-sm rounded-full w-11/12 py-2 px-12 mt-12">
--- components/View.jsx
+++ components/View.jsx
@@ -1,20 +1,8 @@
 import React from "react";
 import { Row, Col } from "react-bootstrap";
-import { FaRegStar } from "react-icons/fa";
+import { FaRegStar, FaTimes } from "react-icons/fa";
 
-const View = ({
-  name,
-  location,
-  description,
-  posted,
-  primary,
-  secondary,
-  album,
-  start,
-  end,
-  song,
-  artist,
-}) => {
+const View = ({ song, description, posted, album, setToggleView }) => {
   return (
     <div className="rounded-3xl bg-gray-200">
       <Row className="w-full m-0 p-0 ">
@@ -26,13 +14,13 @@ const View = ({
           <div className="w-10/12 mt-2">
             <div className="h-2 bg-black w-full" />
             <div className="flex justify-between items-center w-full">
-              <p className="m-0">{start}</p>
-              <p className="m-0">{end}</p>
+              <p className="m-0">PULL FROM SPOTIFY</p>
+              <p className="m-0">PULL FROM SPOTIFY</p>
             </div>
             <div className="flex justify-between items-center w-full">
               <div className="flex justify-center items-start flex-col w-full">
                 <p className="m-0">{song}</p>
-                <p className="m-0">{artist}</p>
+                <p className="m-0">PULL FROM SPOTIFY</p>
               </div>
               <FaRegStar />
             </div>
@@ -40,25 +28,14 @@ const View = ({
         </Col>
         <Col lg={7} className=" p-4">
           <div className="flex justify-between items-center">
-            <p className="m-2 font-bold text-3xl">{name}</p>
-            <p className="m-0 rounded-full bg-pink-700 px-2 py-1 text-white">
-              {location}
-            </p>
+            <p className="m-2 font-bold text-3xl">PULL FROM FIREBASE</p>
+            <FaTimes
+              className="hover:text-red-500 hover:cursor-pointer"
+              onClick={() => setToggleView(false)}
+            />
           </div>
           <p className="m-2">{description}</p>
           <p className="m-2 font-light text-gray-500">{posted}</p>
-          <div className="flex justify-evenly items-center w-full">
-            <img
-              className="rounded-3xl w-1/3"
-              src={primary}
-              alt="Laguna Beach"
-            />
-            <img
-              className="rounded-3xl w-1/3"
-              src={secondary}
-              alt="Laguna Beach"
-            />
-          </div>
         </Col>
       </Row>
     </div>
