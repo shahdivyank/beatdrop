336bffd8c95c95469feba116c80c2501f8e9931d
Mariam Golwalla <mgolw001@ucr.edu>
2023-03-01 10:29:34 -0800

    upload has name from firebase

--- components/Upload.jsx
+++ components/Upload.jsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FaTimes } from "react-icons/fa";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "@/firebase";
 
 const Upload = ({ setToggleUpload }) => {
+  const [name, setName] = useState("");
+
+  useEffect(() => {
+    onAuthStateChanged(auth, async (currentState) => {
+      if (currentState !== null) {
+        setName(currentState.displayName);
+      }
+    });
+  }, []);
+
   return (
     <div className="bg-red-500">
-      HELLO THIS IS TESTING WHERE THE UPLOAD IS
+      {name}
       <FaTimes
         className="hover:text-red-500 hover:cursor-pointer"
         onClick={() => setToggleUpload(false)}
