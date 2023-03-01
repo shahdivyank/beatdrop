a26d866c48811d516a54f5dc18d062ecc259c3cb
william1423 <williamhuang4321@gmail.com>
2023-02-28 18:04:39 -0800

    added gmap Custom Marker

--- components/Gmap.jsx
+++ components/Gmap.jsx
@@ -48,7 +48,12 @@ const Gmap = () => {
         onLoad={onLoad}
         options={{ streetViewControl: false }}
       >
-        <Marker position={{ lat: lat, lng: lng }} />
+        <Marker
+          icon={{
+            path: "M8 12l-4.7023 2.4721.898-5.236L.3916 5.5279l5.2574-.764L8 0l2.3511 4.764 5.2574.7639-3.8043 3.7082.898 5.236z",
+          }}
+          position={{ lat: lat, lng: lng }}
+        />
       </GoogleMap>
     </div>
   ) : (
