89ccf5d712c84d0fbc91eefb57cbd90bda74c078
Divyank Shah <divyank.shah.2016@gmail.com>
2023-02-28 17:38:21 -0800

    convert to axios

--- pages/api/getToken.js
+++ pages/api/getToken.js
@@ -1,18 +1,19 @@
+import axios from "axios";
+
 export default async function handler(req, res) {
-  const authParams = {
-    method: "POST",
+  const body = `grant_type=client_credentials&client_id=${process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID}&client_secret=${process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_SECRET}`;
+
+  const headers = {
     headers: {
       "Content-Type": "application/x-www-form-urlencoded",
     },
-    body: `grant_type=client_credentials&client_id=${process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID}&client_secret=${process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_SECRET}`,
   };
 
-  const response = await fetch(
+  const response = await axios.post(
     "https://accounts.spotify.com/api/token",
-    authParams
+    body,
+    headers
   );
 
-  const token = await response.json();
-
-  res.status(200).json(token.access_token);
+  res.status(200).json(response.data.access_token);
 }
