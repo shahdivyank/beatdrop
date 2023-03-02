2ad7952afc5648fadd39e7049e19aeff1fca710d
Robert Lerias <72523111+bobbyyy57@users.noreply.github.com>
2023-03-02 14:52:02 -0800

    added listing shadow

--- components/Listing.jsx
+++ components/Listing.jsx
@@ -91,7 +91,7 @@ const Listing = () => {
           />
         )}
         {toggleUpload && <Upload setToggleUpload={setToggleUpload} />}
-        <div className="bg-beatdrop-lightgrey w-3.5/12 h-1/2 rounded-4xl py-4 px-3 my-10 mr-10 flex flex-col items-center">
+        <div className="bg-beatdrop-lightgrey shadow-sm w-3.5/12 h-1/2 rounded-4xl py-4 px-3 my-10 mr-10 flex flex-col items-center">
           <div className=" text-white text-sm rounded-full w-11/12">
             <div className="flex justify-evenly">
               <button
