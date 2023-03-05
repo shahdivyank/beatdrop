5996b0b73bac16ad247fb48cc6a5a69f738fbde2
Divyank Shah <divyank.shah.2016@gmail.com>
2023-03-05 01:44:12 -0800

    private drops

--- components/Listing.jsx
+++ components/Listing.jsx
@@ -4,29 +4,23 @@ import View from "./View";
 import Upload from "./Upload";
 import axios from "axios";
 
-const privateSongs = new Array(1).fill({
-  song: "Bob B",
-  band: "Rodrigo",
-  posted: "1 HOUR",
-  username: "Bobby",
-  location: "Riverside, CA",
-  description:
-    "Lorem ipsum dolor idfji maidf midf ndms i fdmkfsjif mdfnij is there a max to the amout of writin g i can mput i ado nt nto know ",
-  hashtags: ["LIVE", "LAUGH", "LOVE", "LIGHT", "LEARN"],
-});
-
-const Listing = () => {
+const Listing = ({ uid }) => {
   const [toggle, setToggle] = useState(0);
   const [toggleView, setToggleView] = useState(false);
   const [toggleUpload, setToggleUpload] = useState(false);
   const [selectedSong, setSelectedSong] = useState({});
   const [token, setToken] = useState("");
   const [publicSongs, setPublicSongs] = useState([]);
+  const [privateSongs, setPrivateSongs] = useState([]);
 
   useEffect(() => {
     axios
       .post("/api/getPublicDrops")
       .then((response) => setPublicSongs(response.data))
+      .catch((error) => console.log(error));
+    axios
+      .post("/api/getPrivateDrops", { uid: uid })
+      .then((response) => setPrivateSongs(response.data))
       .catch((error) => console.log(error));
   }, []);
 
@@ -109,12 +103,24 @@ const Listing = () => {
             <div className="my-4 px-2">
               {privateSongs.map((song, index) => (
                 <div className="border-b-2 border-[#E3E3E3]" key={index}>
-                  <Song
-                    songID={song.song}
-                    band={song.band}
-                    time={song.time}
-                    username={song.username}
-                  />
+                  {token && song.data && (
+                    <Song
+                      songID={song.data.songID}
+                      time={song.data.timestamp}
+                      username={song.data.name}
+                      hashtags={song.data.hashtags}
+                      longitude={song.data.longitude}
+                      latitude={song.data.latitude}
+                      description={song.data.description}
+                      setToggleView={setToggleView}
+                      setSelectedSong={setSelectedSong}
+                      selectedSong={selectedSong}
+                      toggleView={toggleView}
+                      token={token}
+                      name={song.data.name}
+                      likes={song.data.likes}
+                    />
+                  )}
                 </div>
               ))}
             </div>
