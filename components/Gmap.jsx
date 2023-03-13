995fd2c128a73a734fa9aff54af133e94a466dbd
william1423 <williamhuang4321@gmail.com>
2023-03-12 23:03:37 -0700

    merged dev gmap into current branch gmap

--- components/Gmap.jsx
+++ components/Gmap.jsx
@@ -1,8 +1,10 @@
 import React, { useState, useCallback } from "react";
 import { useEffect } from "react";
 import { GoogleMap, useJsApiLoader, Marker } from "@react-google-maps/api";
+import View from "./View";
+import axios from "axios";
 
-const Gmap = () => {
+const Gmap = ({ publicSongs, privateSongs, token }) => {
   const { isLoaded } = useJsApiLoader({
     id: "google-map-script",
     googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
@@ -12,6 +14,35 @@ const Gmap = () => {
   const [lng, setLng] = useState(-117.33161755952241);
   const [zoom, setZoom] = useState(1);
   const [markers, setMarkers] = useState([]);
+  const [viewData, setViewData] = useState({});
+  const [viewToggle, setViewToggle] = useState(false);
+
+  const handleMarkerClick = async (marker) => {
+    console.log("AINT NO WAY");
+    console.log(marker);
+
+    const response = await axios.post("/api/getSong", {
+      songID: marker.data.songID,
+      token: token,
+    });
+
+    setViewData({
+      id: marker.id,
+      song: response.data.song,
+      name: marker.data.name,
+      artist: response.data.artist,
+      externalurl: response.data.externalurl,
+      description: marker.data.description,
+      location: { lat: marker.data.latitude, long: marker.data.longitude },
+      image: response.data.url,
+      time: marker.data.timestamp,
+      hashtags: marker.data.hashtags,
+      dropLikes: marker.data.likes,
+      setToggleView: setViewToggle,
+    });
+
+    setViewToggle(true);
+  };
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -20,11 +51,21 @@ const Gmap = () => {
           setLat(position.coords.latitude);
           setLng(position.coords.longitude);
           setZoom(15);
+          console.log(position.coords.latitude, position.coords.longitude);
         });
       }
     }, 2000);
     return () => clearInterval(interval);
   }, []);
+
+  useEffect(() => {
+    const markers = [];
+    publicSongs.forEach((element) => {
+      markers.push(element);
+    });
+
+    setMarkers(markers);
+  }, [publicSongs]);
 
   const onLoad = useCallback(
     (map) => {
@@ -39,6 +80,24 @@ const Gmap = () => {
 
   return isLoaded ? (
     <div className="w-full h-full">
+      <div className="absolute top-0 left-0 z-[100000] mt-20">
+        {viewToggle && (
+          <View
+            id={viewData.id}
+            song={viewData.song}
+            name={viewData.name}
+            artist={viewData.artist}
+            externalurl={viewData.externalurl}
+            description={viewData.description}
+            location={viewData.location}
+            image={viewData.image}
+            time={viewData.time}
+            hashtags={viewData.hashtags}
+            setToggleView={viewData.setToggleView}
+            dropLikes={viewData.dropLikes}
+          />
+        )}
+      </div>
       <GoogleMap
         mapContainerStyle={{
           width: "100vw",
@@ -48,16 +107,6 @@ const Gmap = () => {
         zoom={zoom}
         onLoad={onLoad}
         options={{ streetViewControl: false, disableDoubleClickZoom: true }}
-        onDblClick={(e) => {
-          setMarkers([
-            ...markers,
-            {
-              id: markers.length + 1,
-              lat: e.latLng.lat(),
-              lng: e.latLng.lng(),
-            },
-          ]);
-        }}
       >
         <Marker
           icon={{
@@ -69,12 +118,14 @@ const Gmap = () => {
           }}
           position={{ lat: lat, lng: lng }}
         />
-        {markers.map((marker) => (
+        {markers.length !== 0 && 
+          markers.map((marker, index) => (
           <Marker
-            key={marker.id}
+            key={index}
+            onClick={() => handleMarkerClick(marker)}
             position={{
-              lat: marker.lat,
-              lng: marker.lng,
+              lat: marker.data.latitude,
+              lng: marker.data.longitude,
             }}
             icon={{
               path: "M336 1049 c-106 -25 -221 -116 -271 -214 -112 -222 6 -500 245 -578 8 -2 38 -59 65 -126 28 -66 52 -121 55 -121 3 0 28 55 55 121 40 96 55 123 75 131 104 40 196 122 239 211 110 228 -20 508 -265 572 -64 16 -138 18 -198 4z m205 -166 c35 -18 64 -43 84 -71 29 -43 30 -47 33 -182 l3 -138 -29 -34 c-27 -29 -36 -33 -82 -33 -46 0 -55 4 -81 33 -22 24 -29 42 -29 73 0 79 92 135 161 99 18 -10 19 -7 19 41 0 112 -65 180 -176 187 -54 4 -70 1 -106 -21 -59 -34 -88 -88 -88 -163 0 -45 3 -55 12 -46 7 7 32 12 56 12 37 0 50 -6 78 -34 24 -24 34 -43 34 -65 0 -68 -53 -121 -121 -121 -22 0 -41 10 -65 34 l-34 34 0 131 c0 110 3 138 19 169 58 113 194 154 312 95z",
