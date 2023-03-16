5ee798b6fe9d3acc81c347c96835e5ef7b8c3c11
Zorten <zergionaruto@hotmail.com>
2023-03-15 19:16:27 -0700

    Fixed cypress test fr this time

--- cypress/e2e/welcome.cy.js
+++ cypress/e2e/welcome.cy.js
@@ -6,6 +6,6 @@ context("Welcome", () => {
   });
 
   it("Find 'hear the world from another’s perspective'", () => {
-    cy.get("h3").contains("hear the world from another perspective");
+    cy.get("h3").contains("hear the world from another’s perspective");
   });
 });
