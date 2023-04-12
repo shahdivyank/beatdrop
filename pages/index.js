5d591229532f364d771890bce4d551e8962aacd4
Robert Lerias <72523111+bobbyyy57@users.noreply.github.com>
2023-04-11 19:41:45 -0700

    prettier and removed import react

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
 import IndexSVG from "@/components/SVGs/IndexSVG";
 import Welcome from "@/components/Welcome";
-import React from "react";
 
 const index = () => {
   return (
     <div className="m-0 p-0 h-screen overflow-hidden">
-      <title>Beatdrop </title>
+      <title>Beatdrop</title>
       <IndexSVG />
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
         <Welcome />
