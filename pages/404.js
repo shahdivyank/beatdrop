c99e151ecab4bad48985511236733304e284d681
Robert Lerias <72523111+bobbyyy57@users.noreply.github.com>
2023-04-18 22:35:03 -0700

    fixed build and testing error

--- pages/404.js
+++ pages/404.js
@@ -1,15 +1,16 @@
-import GraySwoosh from "@/components/SVGs/400Swoosh";
-import Circle400 from "@/components/SVGs/400";
+import GraySwoosh from "@/public/Swoosh400.svg";
+import Circle400 from "@/public/Circle400.svg";
+import Image from "next/image";
 
 export default function Custom404() {
   return (
     <div className="flex items-center justify-center text-white">
       <title>404</title>
       <div className="absolute top-0 left-0 overflow-hidden z-0 bg-beatdrop-lightgrey ">
-        <GraySwoosh />
+        <Image src={GraySwoosh} alt="cloud" draggable={false} />
       </div>
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 mt-4">
-        <Circle400 />
+        <Image src={Circle400} alt="cloud" draggable={false} />
       </div>
 
       <div className="flex flex-col items-center justify-center w-[480px] h-[480px]  z-10 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 m-0 p-0">
--- pages/500.js
+++ pages/500.js
@@ -1,15 +1,16 @@
-import GraySwoosh from "@/components/SVGs/500Swoosh";
-import Circle500 from "@/components/SVGs/500";
+import GraySwoosh from "@/public/Swoosh500.svg";
+import Circle500 from "@/public/Circle500.svg";
+import Image from "next/image";
 
 export default function Custom500() {
   return (
     <div className="flex items-center justify-center text-white">
       <title>500</title>
       <div className="absolute top-0 left-0 overflow-hidden z-0 bg-beatdrop-lightgrey ">
-        <GraySwoosh />
+        <Image src={GraySwoosh} alt="cloud" draggable={false} />
       </div>
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 mt-4">
-        <Circle500 />
+        <Image src={Circle500} alt="cloud" draggable={false} />
       </div>
 
       <div className="flex flex-col items-center justify-center w-[480px] h-[480px]  z-10 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
