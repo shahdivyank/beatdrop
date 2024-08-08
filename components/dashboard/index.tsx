cf25114bed240b11848898b335881251009c8970
shahdivyank <divyank.shah.2016@gmail.com>
2024-08-08 00:46:29 -0700

    add warning

--- components/dashboard/index.tsx
+++ components/dashboard/index.tsx
@@ -1,10 +1,14 @@
-import { View, Text, SafeAreaView } from "react-native";
+import { Text, SafeAreaView } from "react-native";
 import React from "react";
 
 const DashboardScreen = () => {
   return (
     <SafeAreaView>
-      <Text>DashboardScreen</Text>
+      <Text className="bg-red-500">DashboardScreen</Text>
+      <Text>
+        Note: Above Text should have a red background, otherwise Tailwind is not
+        loaded properly!
+      </Text>
     </SafeAreaView>
   );
 };
