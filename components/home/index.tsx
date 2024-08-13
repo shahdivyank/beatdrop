0670e38aec070bc97184aa5b1866d661e54ad410
Zorten <zergionaruto@hotmail.com>
2024-08-12 22:11:23 -0700

    Removed header

--- components/home/index.tsx
+++ components/home/index.tsx
@@ -3,7 +3,7 @@ import { View, Text } from "react-native";
 
 const HomeScreen = () => {
   return (
-    <View>
+    <View className="mt-24">
       <Text>beatdrop</Text>
       <Text>hear the world from another perspective</Text>
       <Link href="/onboarding/0">Sign Up With Spotify</Link>
