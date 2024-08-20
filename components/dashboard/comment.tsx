0e40073f1d4b816907655bf4aa8d452c896d55c2
seanquiambao <slquiambao03@gmail.com>
2024-08-19 17:04:15 -0700

    finished functionality

--- components/dashboard/comment.tsx
+++ components/dashboard/comment.tsx
@@ -1,11 +1,35 @@
 import { View, Pressable, TextInput, InputAccessoryView } from "react-native";
 import { Image, ImageSource } from "expo-image";
 import Send from "@/assets/icons/Send.svg";
+import { beat, drop, comment } from "@/types";
+import { useState } from "react";
 interface props {
   photo: ImageSource;
+  beat: Record<string, never> | (beat & drop);
+  setBeat: any;
 }
 
-const Comment = ({ photo }: props) => {
+const Comment = ({ photo, beat, setBeat }: props) => {
+  const [message, setMessage] = useState("");
+
+  const handlePress = () => {
+    if (message.length === 0) return;
+    console.log(message);
+    const NEWCOMMENT: comment = {
+      timestamp: new Date(),
+      username: "bobby",
+      likes: 150,
+      comment: message,
+      photo: {
+        uri: "https://media.licdn.com/dms/image/C5603AQFRF-WuzzVSPw/profile-displayphoto-shrink_200_200/0/1648079904789?e=2147483647&v=beta&t=iQ5MB_agi9aY0JUDxSXlAEa3icdQWn8l9twByRP5ItQ",
+      },
+    };
+
+    setBeat({
+      ...beat,
+      comments: [NEWCOMMENT, ...(beat.comments ? beat.comments : [])],
+    });
+  };
   return (
     <InputAccessoryView backgroundColor="white">
       <View className="bg-white p-2 border-t border-beatdrop-border/50 flex-row items-center justify-between">
@@ -17,8 +41,12 @@ const Comment = ({ photo }: props) => {
           <TextInput
             className="placeholder:text-beatdrop-placeholder w-9/12 pl-2"
             placeholder="Write a comment"
+            onChangeText={setMessage}
           />
-          <Pressable className="flex flex-col bg-beatdrop-primary rounded-full items-center justify-center w-12 h-12 pr-0.5">
+          <Pressable
+            className="flex flex-col bg-beatdrop-primary rounded-full items-center justify-center p-3"
+            onPress={handlePress}
+          >
             <Image source={Send} style={{ height: 20, width: 20 }} />
           </Pressable>
         </View>
--- components/dashboard/index.tsx
+++ components/dashboard/index.tsx
@@ -92,7 +92,7 @@ const beats: (drop & beat)[] = [
     artist: "G(I)-DLE",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-    comments: comments.slice(0, 1),
+    comments: [],
   },
   {
     uid: "1",
@@ -295,7 +295,7 @@ const DashboardScreen = () => {
                 )}
               </View>
             </View>
-            <Comment photo={comments[2].photo} />
+            <Comment photo={comments[2].photo} beat={beat} setBeat={setBeat} />
           </BottomSheetScrollView>
         )}
       </BottomSheet>
