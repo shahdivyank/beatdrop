8b75807440945196145c9bbdccd16293280844a3
shahdivyank <divyank.shah.2016@gmail.com>
2024-08-31 22:55:18 -0700

    add map functions

--- components/dashboard/index.tsx
+++ components/dashboard/index.tsx
@@ -244,7 +244,7 @@ const DashboardScreen = () => {
         <Text className="p-2">{moment(timestamp).fromNow()}</Text>
       </Pressable>
     ),
-    []
+    [],
   );
 
   const map = useRef<MapView>(null);
@@ -354,7 +354,7 @@ const DashboardScreen = () => {
                 {beat.comments?.map(
                   (
                     { timestamp, photo, likes, username, comment }: comment,
-                    index
+                    index,
                   ) => (
                     <View className="flex flex-row" key={index}>
                       <View className="rounded-full overflow-hidden h-[50px] w-[50px]">
@@ -379,7 +379,7 @@ const DashboardScreen = () => {
                         <Text className="">{likes}</Text>
                       </View>
                     </View>
-                  )
+                  ),
                 )}
               </View>
             </View>
