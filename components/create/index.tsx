a4bc26a3a019e608c48b57f27c37c13d1dcfb14a
shahdivyank <divyank.shah.2016@gmail.com>
2024-08-31 21:04:44 -0700

    remove animation

--- components/create/index.tsx
+++ components/create/index.tsx
@@ -4,13 +4,6 @@ import BottomSheet from "@gorhom/bottom-sheet";
 import Details from "./details";
 import Search from "./search";
 import { beat } from "@/types";
-import Animated, {
-  Easing,
-  SlideInLeft,
-  SlideInRight,
-  SlideOutLeft,
-  SlideOutRight,
-} from "react-native-reanimated";
 
 const CreateScreen = () => {
   const ref = useRef<BottomSheet>(null);
@@ -36,38 +29,17 @@ const CreateScreen = () => {
       <BottomSheet ref={ref} snapPoints={snapPoints}>
         <View className="p-3 flex justify-between items-center">
           {state === 0 && (
-            <Animated.View
-              className={"w-full"}
-              entering={SlideInLeft.duration(225).easing(
-                Easing.bezierFn(0.56, 0.15, 0.35, 0.78),
-              )}
-              exiting={SlideOutLeft.duration(175).easing(
-                Easing.bezierFn(0.56, 0.15, 0.35, 0.78),
-              )}
-            >
-              <Search setBeat={setBeat} handleNext={() => setState(1)} />
-            </Animated.View>
+            <Search setBeat={setBeat} handleNext={() => setState(1)} />
           )}
-
           {state === 1 && (
-            <Animated.View
-              className={"w-full"}
-              entering={SlideInRight.duration(225).easing(
-                Easing.bezierFn(0.56, 0.15, 0.35, 0.78),
-              )}
-              exiting={SlideOutRight.duration(175).easing(
-                Easing.bezierFn(0.56, 0.15, 0.35, 0.78),
-              )}
-            >
-              <Details
-                beat={beat}
-                handleBack={() => setState(0)}
-                description={description}
-                setDescription={setDescription}
-                addTag={addTag}
-                tags={tags}
-              />
-            </Animated.View>
+            <Details
+              beat={beat}
+              handleBack={() => setState(0)}
+              description={description}
+              setDescription={setDescription}
+              addTag={addTag}
+              tags={tags}
+            />
           )}
         </View>
       </BottomSheet>
