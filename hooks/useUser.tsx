eef63a1e6baf64a30ca909012bcad27b957aff4d
shahdivyank <divyank.shah.2016@gmail.com>
2024-08-31 23:40:50 -0700

    user store

---
+++ hooks/useUser.tsx
@@ -0,0 +1,9 @@
+import { create } from "zustand";
+
+export const useUser = create(() => ({
+  name: "Divyank Shah",
+  beatdrops: 21,
+  friends: 2,
+  image:
+    "https://media.licdn.com/dms/image/C5603AQGGCb3sfU37yw/profile-displayphoto-shrink_200_200/0/1643607680906?e=2147483647&v=beta&t=3O3YNLDDQJ8kjWiFRtLQJRR-gj5JRN6hd6eerzGHdnY",
+}));
