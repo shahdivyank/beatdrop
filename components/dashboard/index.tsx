6f38c7b25086cee152e5e9113732d3e72531ca64
shahdivyank <divyank.shah.2016@gmail.com>
2024-09-07 01:29:52 -0700

    persisted comments

--- components/dashboard/index.tsx
+++ components/dashboard/index.tsx
@@ -128,7 +128,7 @@ const DashboardScreen = () => {
         <Text className="p-2">{moment(timestamp).fromNow()}</Text>
       </Pressable>
     ),
-    [],
+    []
   );
 
   const map = useRef<MapView>(null);
@@ -235,11 +235,11 @@ const DashboardScreen = () => {
               {beat.comments?.map(
                 (
                   { timestamp, photo, likes, username, comment }: comment,
-                  index,
+                  index
                 ) => (
                   <View className="flex flex-row" key={index}>
-                    <View className="rounded-full overflow-hidden h-[50px] w-[50px]">
-                      <Image source={photo} style={{ height: 50, width: 50 }} />
+                    <View className="rounded-full overflow-hidden h-[40px] w-[40px]">
+                      <Image source={photo} style={{ height: 40, width: 40 }} />
                     </View>
                     <View className="px-2 flex-1">
                       <View className="flex flex-row items-center">
@@ -257,7 +257,7 @@ const DashboardScreen = () => {
                       <Text className="">{likes}</Text>
                     </View>
                   </View>
-                ),
+                )
               )}
             </View>
             <Comment beat={beat} setBeat={setBeat} />
---
+++ hooks/useDrops.ts
@@ -0,0 +1,28 @@
+import { beats } from "@/db/__mock__/drops";
+import { beatdrop, comment } from "@/types";
+import { create } from "zustand";
+
+interface DropsState {
+  drops: beatdrop[];
+  addDrop: (drop: beatdrop) => void;
+  addComment: (id: string, comment: comment) => void;
+}
+
+export const useDrops = create<DropsState>()((set) => ({
+  drops: beats,
+
+  addDrop: (drop: beatdrop) =>
+    set(({ drops }) => ({ drops: [drop, ...drops] })),
+
+  addComment: (id: string, comment: comment) =>
+    set(({ drops }) => ({
+      drops: drops.map((drop) => {
+        const { did } = drop;
+        if (did === id) {
+          return { ...drop, comments: [...(drop.comments as []), comment] };
+        } else {
+          return drop;
+        }
+      }),
+    })),
+}));
